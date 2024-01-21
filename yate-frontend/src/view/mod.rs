6adedec73eb4592f041933301a3e3bf53d7e5562
aserowy <serowy@hotmail.com>
2024-01-21 09:02:18 +0100

    frontend: removed unused pub for mods in view
    Signed-off-by: aserowy <serowy@hotmail.com>

--- yate-frontend/src/view/mod.rs
+++ yate-frontend/src/view/mod.rs
@@ -3,11 +3,11 @@ use ratatui::Frame;
 use crate::{layout::AppLayout, model::Model};
 
 mod buffer;
-pub mod commandline;
-pub mod current_directory;
-pub mod parent_directory;
-pub mod preview;
-pub mod statusline;
+mod commandline;
+mod current_directory;
+mod parent_directory;
+mod preview;
+mod statusline;
 
 pub fn view(model: &mut Model, frame: &mut Frame, layout: &AppLayout) {
     commandline::view(model, frame, layout.commandline);
