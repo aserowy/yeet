dfbe45b1feef508588109091c27ee636525e95ee
aserowy <serowy@hotmail.com>
2024-09-11 09:07:24 +0200

    frontend: introduced syntax module for highlighting
    Signed-off-by: aserowy <serowy@hotmail.com>

--- yeet-frontend/src/task/syntax.rs
+++ yeet-frontend/src/task/syntax.rs
@@ -1,7 +1,15 @@
 use std::path::Path;
 
-
+use tokio::fs;
 
 pub async fn highlight(path: &Path) -> Option<String> {
-    todo!()
+    match fs::read_to_string(path).await {
+        Ok(content) => {
+            Some(content)
+        },
+        Err(err) => {
+            tracing::error!("reading file failed: {:?} {:?}", path, err);
+            None
+        }
+    }
 }
